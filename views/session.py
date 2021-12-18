ab48d33bd43e1ea3686d5d5b4f620ed4459021d9
yyjlincoln <yyjlincoln@gmail.com>
2021-12-19 00:13:09 +1100

    Update: Added chatterbot as a submodule

--- views/session.py
+++ views/session.py
@@ -28,5 +28,12 @@ def connect(sessionId, discordId=None):  # Client init - get a new identity
         print('Creating a new session')
         sessionId = core.session.newSession(discordId=discordId)
         print(f'New sessionId: {sessionId}')
-    
+
     return returnMessage(0, sessionId=sessionId, sessionInfo=core.db2json.Session(core.session.getSession(sessionId)))
+
+
+@API.on('discord-verification')
+@parseData
+def discordVerification(discordId):
+    print(f'Discord verification for {discordId}')
+    return returnMessage(0, hasJoinedDiscord=True)
