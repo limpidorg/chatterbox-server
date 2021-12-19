77215c25fd355be32ef899b6061b2e383a179c99
EsprIx <46829058+EsprIx@users.noreply.github.com>
2021-12-19 12:48:07 +0100

    sends message

--- core/chat.py
+++ core/chat.py
@@ -2,6 +2,7 @@ from database import Session, Chat
 import secrets
 import core.notifications
 import core.session
+from integrations.discordbot import getDiscordId, sendMessage
 from utils import returnMessage
 
 
@@ -30,6 +31,7 @@ def newChat(sessionIds):
     except:
         return None
 
+
 def deleteChat(chatId):
     chat = getChat(chatId)
     if chat:
@@ -42,19 +44,30 @@ def deleteChat(chatId):
         return True
     return False
 
+
 def initiateChat(chatId):
-    'Initiates a chat after matching'
+    "Initiates a chat after matching"
     chat = getChat(chatId)
     if chat:
         for sessionId in chat.sessionIds:
             session = core.session.getSession(sessionId)
             if session:
+                discordId = session.discordId
+                discord_internal_id = getDiscordId(discordId)
+
                 session.chatId = chatId
                 session.save()
-            core.notifications.sendNotificationToSession(
-                sessionId, 'new-chat-found', returnMessage(0, chatId=chatId))
+            core.notifications.sendNotificationToSession(sessionId, "new-chat-found", returnMessage(0, chatId=chatId))
+
+            message = {
+                "title": "Hurry up",
+                "description": "We just found your new chatling! It could be the beginning of a great adventure 💙",
+            }
+
+            sendMessage(discord_internal_id, message)
         return True
     return False
 
+
 def joinChat(sessionId, chatId):
-    pass    
+    pass
